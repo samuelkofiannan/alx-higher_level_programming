b602817d5e6b4b60653df064816a195fa6cd44b1
Kofi <qouphieligal@gmail.com>
2024-01-21 08:14:13 -0800

    Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 
 def magic_calculation(a, b):
-    """Impor bytecode provided by Holberton School."""
+    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
