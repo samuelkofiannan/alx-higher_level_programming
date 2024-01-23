62fb30befbf0d99e0a03491d5a8d819335cf2fc8
Kofi <qouphieligal@gmail.com>
2024-01-22 20:37:57 -0800

    Python - Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-
-""" A function that executes a function safely.
-    Returns:
-        The results of the function.
-    Otherwise - None
-"""
-
 def safe_function(fct, *args):
     try:
         num = fct(*args)
