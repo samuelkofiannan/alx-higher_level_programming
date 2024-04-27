76625dd41c30c1529f0ad08dea01ce6e24f93fdf
Kofi <qouphieligal@gmail.com>
2024-04-26 19:58:14 -0700

    Python - Network #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
+"""Python script that takes in a URL
+Sends a request to the URL and displays the body of the response."""
 
 import sys
 import requests
