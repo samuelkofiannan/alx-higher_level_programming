700761ea2e5bb4a8aece1e57eebfcbadc2df5874
Kofi <qouphieligal@gmail.com>
2024-04-26 17:43:54 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,23 @@
-
 #!/usr/bin/python3
 """Lists states"""
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+def lists_states(user, password, database, search_query):
+    """Lists states in the database"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
     query = """
-SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
-    query = query.format(argv[4])
+    SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(search_query)
     cur.execute(query)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
     conn.close()
+
+if __name__ == "__main__":
+    lists_states(argv[1], argv[2], argv[3], argv[4])
