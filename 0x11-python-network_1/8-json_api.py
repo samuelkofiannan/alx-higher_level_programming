dddfacd966ec63498f2be51e9cafafa2988f3f09
Kofi <qouphieligal@gmail.com>
2024-04-26 20:01:51 -0700

    Python - Network #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""a Python script that takes in a letter and sends
-a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+"""A Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter"""
 
 if __name__ == '__main__':
     from requests import post
