671ae3018c2b41364868f70bc04ce975a41f3c6d
samuelkofiannan <qouphieligal@gmail.com>
2024-05-24 16:10:33 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 // A script that prints all characters of a Star Wars movie
 
 const request = require('request');
-const BASE_URL = 'https://swapi-api.alx-tools.com/api/films';
+const BASE_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
   request(`${BASE_URL}/films/${process.argv[2]}/`, (err, res, body) => {
